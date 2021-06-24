108478392691262c8d59d54cab4c632e3646cb8a
Dustin Leatherman <rahlord1391@gmail.com>
2021-06-24 15:31:04 -0500

    README

--- IndependentResearch/ecoli/script.R
+++ IndependentResearch/ecoli/script.R
@@ -4,12 +4,13 @@ beach_weather_data <- read.csv("~/projects/portfolio/IndependentResearch/ecoli/d
 beach_water_data <- read.csv("~/projects/portfolio/IndependentResearch/ecoli/data/Beach_Water_Quality_Automated_Sensors.csv")
 
 require(tidyverse)
+require(sqldf)
 
 dist.haversine <- function(lat1, lon1, lat2, lon2) {
   # radius of earth in km
   R <- 6371
   
-  # convert to radians for this to work
+  # convert inputs to radians
   toRad <- pi/180
   lat1 <- lat1 * toRad
   lat2 <- lat2 * toRad
@@ -37,7 +38,13 @@ beach_water_data <-
 beach_sensor_data <-
     beach_water_data %>% 
     # The beaches represented in the weather dataset is a subset of those in the water dataset, hence the left join
-    left_join(beach_weather_data, by = "Measurement.ID.std", suffix = c(".weather", ".water"))
+    left_join(beach_weather_data, by = "Measurement.ID.std", suffix = c(".weather", ".water")) %>%
+    # since we have some beaches with missing water and temperature data,
+    # this makes sure we join correctly
+    mutate(
+      Timestamp.Key = coalesce(Measurement.Timestamp.weather, Measurement.Timestamp.water)
+    ) %>% 
+  select(-Measurement.Timestamp.weather, -Measurement.Timestamp.water)
 
 # clean lab data
 data <- 
@@ -52,11 +59,11 @@ data <-
                       "Margaret T Burroughs \\(31st\\)" = "Margaret T Burroughs")
         ),
       " Beach"),
-    Timestamp.key = coalesce(DNA.Sample.Timestamp, Culture.Sample.1.Timestamp, Culture.Sample.2.Timestamp)
+    Timestamp.Key = coalesce(DNA.Sample.Timestamp, Culture.Sample.1.Timestamp, Culture.Sample.2.Timestamp)
   ) %>% 
   # since beach_sensor_data joins on Timestamps, picking either measurement timestamp works for this join.
   # we'll pick water though since it was the left set in the join above
-  left_join(beach_sensor_data, by = c("Beach" = "Beach.Name", "Timestamp.key" = "Measurement.Timestamp.water"))
+  left_join(beach_sensor_data, by = c("Beach" = "Beach.Name", "Timestamp.Key" = "Timestamp.Key"))
 
 # Items to fix
 # 1. Correct beach identification for 31st st and Jarvis Beaches (check)
@@ -76,12 +83,38 @@ a <-
   mutate(
     dist = dist.haversine(Latitude.A, Longitude.A, Latitude.B, Longitude.B)
   )
+# get minumum distance for each beach set, then get the minumum of those.
+# This could probably be done in a single command but I was just trying to get this to work
+minA <- a %>% group_by(A) %>% summarise(min.A = min(dist)) %>% rename(Beach = A)
+minB <- a %>% group_by(B) %>% summarise(min.B = min(dist)) %>% rename(Beach = B)
+df <- 
+  minA %>% 
+    inner_join(minB, by = "Beach") %>% 
+    mutate(min = pmin(min.A, min.B))
 
+# dplyr doesn't do conditional joins (A.id2 = B.id2 OR A.id= B.id) so using sql here.
+# Get the beach names from each set and don't pick the current beach
+# since the closest beach will always be the current beach by distance standards
+sql <- "
+  select 
+    df.Beach
+    , B as `Closest.Beach1`
+    , A as `Closest.Beach2`
+    , min as DistanceToClosestBeach 
+  from df 
+    inner join a on 
+        df.min = a.dist and (df.Beach = a.A or df.Beach = a.B)
+"
+
+# join this back into our data so it can be used.
 data <- 
-  a %>% 
-    group_by(A) %>% 
-    summarise(min = min(dist)) %>% 
-    inner_join(a, by = c("A" = "A", "min" = "dist")) %>% 
-    # bring distance just in case we want it to include it for penalizing the algorithm
-    select(Beach = A, Closest.Beach = B, DistanceToClosestBeach = min) %>% 
-    inner_join(data, by = "Beach")
+  sqldf(sql) %>% 
+  mutate(
+    Closest.Beach = ifelse(Beach == Closest.Beach1, Closest.Beach2, Closest.Beach1)
+  ) %>% 
+  select(Beach, Closest.Beach, DistanceToClosestBeach) %>% 
+  inner_join(data, by = "Beach")
+
+
+# It turns out that the timestamp periods for the sensor data do not overlap for a significant amount of time with the ecoli measurements.
+# What data did they base predictions off of if most of the data does not apply?
